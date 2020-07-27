00f65c7e0b44eb407a0a421241343e75a41892cc
ellamusen <dani_1997@live.dk>
2020-07-27 14:39:11 +0200

    Added Menu class

---
+++ src/com/company/Menu.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    List<String> menuList = new ArrayList<String>();
+
+    public Menu(List<String> menuList) {
+        this.menuList = menuList;
+    }
+}
+
