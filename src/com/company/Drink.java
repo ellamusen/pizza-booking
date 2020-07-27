a71a4ae4aed8a2738b2986868879411d0c7e6bea
ellamusen <dani_1997@live.dk>
2020-07-27 16:00:20 +0200

    Added Food and Drink classes

---
+++ src/com/company/Drink.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Drink {
+
+    // Members / values to class
+    public int id;
+    public String name;
+    public double price_small;
+    public double price_big;
+
+    // Methods
+
+}
---
+++ src/com/company/Food.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Food {
+
+    // Members / values to class
+    public int id;
+    public String name;
+    public double price_regular;
+    public double price_family;
+    public double price_giant;
+
+    // Methods
+
+}
